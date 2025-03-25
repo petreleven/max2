53cea9605de7844b6c29a4fc9c08a70e86094bcb
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2025-03-25 18:47:49 +0300

    Update tourismApplinks.py

--- tourismapp/tourismApplinks.py
+++ tourismapp/tourismApplinks.py
@@ -3,9 +3,11 @@ from.views import home
 from.views import signin
 from.views import signup
 from.views import Signout
+from.views import destination
 urlpatterns = [
 path("" , home,name="home"),
 path("signin" , signin ,name="signin"),
 path("signup" , signup , name="signup"),
 path("Signout" , Signout , name="Signout"),
+  path("destination" , destination ,name="destination")
 ]
